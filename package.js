7cdcfc26a405c4feafbacc7dc2494c3708fc13e7
Lou Langholtz <llangholtz@silvervue.com>
2014-08-07 19:27:47 -0600

    Minor changes to be more consistent with the JavaScript style.

--- package.js
+++ package.js
@@ -1,12 +1,12 @@
 Package.describe({
-  summary: "Timezone support for moment.js, packaged for Meteor. See http://momentjs.com/timezone. Currently includes all timezone data files."
+  summary: 'Timezone support for moment.js, packaged for Meteor. See http://momentjs.com/timezone. Currently includes all timezone data files.'
 });
 
 Package.on_use(function (api, where) {
-  where = where || ['client', 'server']
-  api.use("moment", where);
+  where = where || ['client', 'server'];
+  api.use('moment', where);
   if (api.imply) {
-    api.imply("moment", where);
+    api.imply('moment', where);
   }
   api.add_files('require-shim.js', where);
   api.add_files('lib/moment-timezone/moment-timezone.js', where);
